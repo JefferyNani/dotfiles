be2dc04de842eff8784eb4c8f980b62ed58a5dcc
Jeffery <95682339+JefferyNani@users.noreply.github.com>
2023-01-13 22:36:53 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "Enter your AUR Helper : (eg : yay paru etc.) (no pamac , fuck pamac , yay~chad)" aur
+read -p "Enter your AUR Helper : (eg : yay paru etc. yay=chad ) (no pamac , fuck pamac)" aur
 sudo pacman -Rdd libxft
 $aur -S libxft-bgra-git ttf-joypixels ttf-iosevka-nerd nitrogen maim flameshot dunst pamixer xclip 
 picom-jonaburg-fix
